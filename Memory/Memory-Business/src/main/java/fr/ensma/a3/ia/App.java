7dc6000c68825333e5ca71a3362b868d71ad3978
Hamster-Furtif <lakartoonerie_dutronc@yahoo.fr>
2021-01-05 16:56:40 +0100

    Create Business maven project

---
+++ Memory/Memory-Business/src/main/java/fr/ensma/a3/ia/App.java
@@ -0,0 +1,17 @@
+package fr.ensma.a3.ia;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+    
+}
