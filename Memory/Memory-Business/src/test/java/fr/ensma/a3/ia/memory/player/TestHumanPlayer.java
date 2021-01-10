3dd685d2c4d07da2948f7b307e46031a1c9d76e5
Lucie Mosnier <mosnier.lucie@hotmail.fr>
2021-01-10 23:19:05 +0100

    Test AbstractPlayer (through HumanPlayer) in process

--- Memory/Memory-Business/src/test/java/fr/ensma/a3/ia/memory/player/TestHumanPlayer.java
+++ Memory/Memory-Business/src/test/java/fr/ensma/a3/ia/memory/player/TestHumanPlayer.java
@@ -1,20 +1,47 @@
 package fr.ensma.a3.ia.memory.player;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.ensma.a3.ia.memory.Game;
+import fr.ensma.a3.ia.memory.item.Item;
+import fr.ensma.a3.ia.memory.player.playerstate.IPlayerState;
+import fr.ensma.a3.ia.memory.player.playerstate.Turned0;
+import fr.ensma.a3.ia.memory.player.playerstate.Turned1;
+import fr.ensma.a3.ia.memory.player.playerstate.Waiting;
+import fr.ensma.a3.ia.memory.table.Tile;
+import fr.ensma.a3.ia.memory.table.card.Card;
+
 public class TestHumanPlayer {
 	
-	private HumanPlayer cunegonde;
+	private AbstractPlayer cunegonde;
+	final private Waiting stateWaiting = new Waiting(cunegonde);
+	final private Turned0 stateTurned0 = new Turned0(cunegonde);
+	final private Turned1 stateTurned1 = new Turned1(cunegonde);
+	
+	@Mocked
+	private List<Card> thecards;
+	private List<Item> theinventory;
+	private Tile theturned_tile;
+	protected Game game1;
+	protected IPlayerState thecurrentState;
 	
 	@Before
 	public void initTest() {
 		cunegonde = new HumanPlayer("Cunégonde");
+		cunegonde.setGame(game1);
 	}
 	
 	@Test
 	public void T00_testConstructeurAccesseur() {
 		Assert.assertTrue(cunegonde.getID() == "Cunégonde");
+		Assert.assertEquals(stateWaiting, cunegonde.getState());
+		Assert.assertNotNull(cunegonde.getCards());
+		Assert.assertNotNull(cunegonde.getInventory());
+		Assert.assertEquals(game1, cunegonde.getGame());
+		
 	}
 }
